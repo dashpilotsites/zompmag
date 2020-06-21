361942888946b16acd97069f98b4e507063e59d7
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-21 16:44:52 +0200

    Added some styling

--- src/App.svelte
+++ src/App.svelte
@@ -3,27 +3,63 @@
   import marked from "marked";
   const apiURL = "https://zompmag-nu.vercel.app/api/data.json";
   let data = [];
+  data.entries = [];
 
   onMount(async function() {
+    marked.setOptions({
+      breaks: true
+    });
     const response = await fetch(apiURL);
     data = await response.json();
   });
 </script>
 
+<header>
+  <h1>Zompmag</h1>
+</header>
 <main>
   {#each data.entries as item}
     <section>
-      <h1>{item.title}</h1>
-      <p>
-        {@html marked(item.body)}
-      </p>
+      <div class="content">
+        <h1>{item.title}</h1>
+        <p>
+          {@html marked(item.body)}
+        </p>
+      </div>
     </section>
   {/each}
 </main>
 
 <style>
-  main {
-    max-width: 600px;
+  p {
+    line-height: 1.5em;
+  }
+  h1 {
+    font: 400 45px/1.5 "Playfair Display", Georgia, serif;
+    margin-bottom: 20px;
+  }
+
+  main,
+  p {
+    font: 300 18px/1.7 "Inter", Verdana, Helvetica, sans-serif;
+  }
+
+  .content {
+    max-width: 700px;
     margin: 0 auto;
+  }
+
+  header {
+    text-align: center;
+    padding: 50px;
+  }
+
+  header h1 {
+    margin-bottom: 0;
+  }
+
+  section {
+    padding: 50px;
+    border-bottom: 1px solid #ddd;
   }
 </style>
